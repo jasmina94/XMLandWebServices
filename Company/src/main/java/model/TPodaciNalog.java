fceeabc9eb5c6e56f4fd9c36eb13f07952fcb942
Jelena K <jelenakalabic@gmail.com>
2017-06-15 18:15:14 +0200

    Izgnerisan model na osnovu seme.

---
+++ Company/src/main/java/model/TPodaciNalog.java
@@ -0,0 +1,183 @@
+
+package model;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Java class for TPodaciNalog complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="TPodaciNalog">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="datum_valute" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *         &lt;element name="iznos">
+ *           &lt;complexType>
+ *             &lt;simpleContent>
+ *               &lt;extension base="&lt;http://www.ftn.uns.ac.rs/tipovi>TIznos">
+ *                 &lt;attribute name="valuta" type="{http://www.ftn.uns.ac.rs/tipovi}TOznakaValute" />
+ *               &lt;/extension>
+ *             &lt;/simpleContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "TPodaciNalog", namespace = "http://www.ftn.uns.ac.rs/tipovi", propOrder = {
+    "datumValute",
+    "iznos"
+})
+public class TPodaciNalog {
+
+    @XmlElement(name = "datum_valute", namespace = "http://www.ftn.uns.ac.rs/tipovi", required = true)
+    @XmlSchemaType(name = "date")
+    protected XMLGregorianCalendar datumValute;
+    @XmlElement(namespace = "http://www.ftn.uns.ac.rs/tipovi", required = true)
+    protected TPodaciNalog.Iznos iznos;
+
+    /**
+     * Gets the value of the datumValute property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDatumValute() {
+        return datumValute;
+    }
+
+    /**
+     * Sets the value of the datumValute property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDatumValute(XMLGregorianCalendar value) {
+        this.datumValute = value;
+    }
+
+    /**
+     * Gets the value of the iznos property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link TPodaciNalog.Iznos }
+     *     
+     */
+    public TPodaciNalog.Iznos getIznos() {
+        return iznos;
+    }
+
+    /**
+     * Sets the value of the iznos property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link TPodaciNalog.Iznos }
+     *     
+     */
+    public void setIznos(TPodaciNalog.Iznos value) {
+        this.iznos = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;simpleContent>
+     *     &lt;extension base="&lt;http://www.ftn.uns.ac.rs/tipovi>TIznos">
+     *       &lt;attribute name="valuta" type="{http://www.ftn.uns.ac.rs/tipovi}TOznakaValute" />
+     *     &lt;/extension>
+     *   &lt;/simpleContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "value"
+    })
+    public static class Iznos {
+
+        @XmlValue
+        protected BigDecimal value;
+        @XmlAttribute(name = "valuta")
+        protected TOznakaValute valuta;
+
+        /**
+         * Gets the value of the value property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link BigDecimal }
+         *     
+         */
+        public BigDecimal getValue() {
+            return value;
+        }
+
+        /**
+         * Sets the value of the value property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link BigDecimal }
+         *     
+         */
+        public void setValue(BigDecimal value) {
+            this.value = value;
+        }
+
+        /**
+         * Gets the value of the valuta property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link TOznakaValute }
+         *     
+         */
+        public TOznakaValute getValuta() {
+            return valuta;
+        }
+
+        /**
+         * Sets the value of the valuta property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link TOznakaValute }
+         *     
+         */
+        public void setValuta(TOznakaValute value) {
+            this.valuta = value;
+        }
+
+    }
+
+}
