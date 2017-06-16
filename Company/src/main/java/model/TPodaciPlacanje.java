269b0d5e57630331a49b9cfa2c7e8c71a7cf05dc
Ana <1994.ana@gmail.com>
2017-06-16 12:10:36 +0200

    Revert "Zapoceto pripemanje modela za cuvanje u bazu."
    This reverts commit d5976b9729431a7621ecb7a74ce60c9eb6ba3193.

---
+++ Company/src/main/java/model/TPodaciPlacanje.java
@@ -0,0 +1,103 @@
+
+package model;
+
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for TPodaciPlacanje complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="TPodaciPlacanje">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="model">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}positiveInteger">
+ *               &lt;totalDigits value="2"/>
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *         &lt;element name="poziv_na_broj">
+ *           &lt;simpleType>
+ *             &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *               &lt;maxLength value="20"/>
+ *             &lt;/restriction>
+ *           &lt;/simpleType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "TPodaciPlacanje", namespace = "http://www.ftn.uns.ac.rs/tipovi", propOrder = {
+    "model",
+    "pozivNaBroj"
+})
+public class TPodaciPlacanje {
+
+    @XmlElement(namespace = "http://www.ftn.uns.ac.rs/tipovi", required = true)
+    protected BigInteger model;
+    @XmlElement(name = "poziv_na_broj", namespace = "http://www.ftn.uns.ac.rs/tipovi", required = true)
+    protected String pozivNaBroj;
+
+    /**
+     * Gets the value of the model property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getModel() {
+        return model;
+    }
+
+    /**
+     * Sets the value of the model property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setModel(BigInteger value) {
+        this.model = value;
+    }
+
+    /**
+     * Gets the value of the pozivNaBroj property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPozivNaBroj() {
+        return pozivNaBroj;
+    }
+
+    /**
+     * Sets the value of the pozivNaBroj property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPozivNaBroj(String value) {
+        this.pozivNaBroj = value;
+    }
+
+}
