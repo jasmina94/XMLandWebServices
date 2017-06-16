3ff624a6a58c68b7c6e1053d59f3e8c9856ff531
Jelena K <jelenakalabic@gmail.com>
2017-06-16 17:19:44 +0200

    Dodate sve seme potrebne za NBS. Generisan objektni model za mt900.

---
+++ NBS/src/main/java/com/ftn/model/ObjectFactory.java
@@ -0,0 +1,121 @@
+
+package com.ftn.model;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.ftn.model package.
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and com.ftn.model
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.ftn.model
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Mt900 }
+     * 
+     */
+    public Mt900 createMt900() {
+        return new Mt900();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciNalog }
+     * 
+     */
+    public TPodaciNalog createTPodaciNalog() {
+        return new TPodaciNalog();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciBanka }
+     * 
+     */
+    public TPodaciBanka createTPodaciBanka() {
+        return new TPodaciBanka();
+    }
+
+    /**
+     * Create an instance of {@link Mt900 .PodaciONalogu }
+     * 
+     */
+    public Mt900 .PodaciONalogu createMt900PodaciONalogu() {
+        return new Mt900 .PodaciONalogu();
+    }
+
+
+    /**
+     * Create an instance of {@link TPromene }
+     * 
+     */
+    public TPromene createTPromene() {
+        return new TPromene();
+    }
+
+    /**
+     * Create an instance of {@link TPrenosUcesnik }
+     * 
+     */
+    public TPrenosUcesnik createTPrenosUcesnik() {
+        return new TPrenosUcesnik();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciOPrenosu }
+     * 
+     */
+    public TPodaciOPrenosu createTPodaciOPrenosu() {
+        return new TPodaciOPrenosu();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciSubjekt }
+     * 
+     */
+    public TPodaciSubjekt createTPodaciSubjekt() {
+        return new TPodaciSubjekt();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciPlacanje }
+     * 
+     */
+    public TPodaciPlacanje createTPodaciPlacanje() {
+        return new TPodaciPlacanje();
+    }
+
+    /**
+     * Create an instance of {@link TPravnoLice }
+     * 
+     */
+    public TPravnoLice createTPravnoLice() {
+        return new TPravnoLice();
+    }
+
+    /**
+     * Create an instance of {@link TPodaciNalog.Iznos }
+     * 
+     */
+    public TPodaciNalog.Iznos createTPodaciNalogIznos() {
+        return new TPodaciNalog.Iznos();
+    }
+
+}
