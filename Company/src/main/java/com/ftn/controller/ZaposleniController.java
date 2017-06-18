fbd41297e8d58b3e68eb72ee46e4f201a839c9cb
Ana <1994.ana@gmail.com>
2017-06-18 11:47:17 +0200

    Implementirani servis i kontroler za zaposlene.

--- Company/src/main/java/com/ftn/controller/ZaposleniController.java
+++ Company/src/main/java/com/ftn/controller/ZaposleniController.java
@@ -1,7 +1,15 @@
 package com.ftn.controller;
 
+import com.ftn.service.ZaposleniService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by Olivera on 17.6.2017..
@@ -10,5 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/employees")
 public class ZaposleniController {
 
+    private ZaposleniService zaposleniService;
 
+    @Autowired
+    public ZaposleniController(ZaposleniService zaposleniService) {
+        this.zaposleniService = zaposleniService;
+    }
+
+    @Transactional
+    @GetMapping
+    public ResponseEntity read() {
+        return new ResponseEntity<>(zaposleniService.read(), HttpStatus.OK);
+    }
 }
