b92c26a49ba5cc1cf70fdd003073158f5a2f9e5d
Ana <1994.ana@gmail.com>
2017-06-19 18:56:31 +0200

    Obrisani suvisni fajlovi,

--- Company/src/webapp/service/zaposleniService.js
+++
@@ -1,7 +0,0 @@
-app.service('zaposleniService', function($http){
-    return {
-        read: function (onSuccess, onError) {
-            $http.get('/api/employees').then(onSuccess, onError);
-        }
-    }
-});
