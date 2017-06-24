a4a4fb1ce844ac204e09887cab23b380ad466e5a
Jasmina <jasmina.eminovski@gmail.com>
2017-06-24 20:27:17 +0200

    Ispravljena greska za insert2.sql

--- Banka/src/main/resources/init2.sql
+++ Banka/src/main/resources/init2.sql
@@ -1,3 +1,3 @@
 INSERT INTO `quatrofantasticoxmlbank2`.`banka`(`swiftkod`,`naziv`,`racun_banke`,`sifra`) VALUES("VBUBRS22","Vojvodjanska Banka Novi Sad", "265-1090310000009-72", 265);
 
-INSERT INTO `quatrofantasticoxmlbank1`.`racun`(`broj_racuna`,`saldo`,`banka_id`) VALUES ("c",0.0,1), ("205-2234522100015-72",0.0,1), ("205-2234522100015-72",0.0,1);
+INSERT INTO `quatrofantasticoxmlbank1`.`racun`(`broj_racuna`,`saldo`,`banka_id`) VALUES ("265-2234522100014-72",0.0,1), ("265-2234522100015-72",0.0,1), ("265-2234522100016-72",0.0,1);
