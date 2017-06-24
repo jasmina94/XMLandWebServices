107990b5c6b56f9ddeac054041d24c3688077a11
Zlatan <zlajox@gmail.com>
2017-06-24 19:41:14 +0200

    Dodati endpointi 102 103 900 910

--- Banka/src/main/java/rs/ac/uns/ftn/endpoint/WebServiceConfig.java
+++ Banka/src/main/java/rs/ac/uns/ftn/endpoint/WebServiceConfig.java
@@ -7,10 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.soap.server.endpoint.SoapFaultDefinition;
+import org.springframework.ws.soap.server.endpoint.SoapFaultMappingExceptionResolver;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+import rs.ac.uns.ftn.exception.DetailSoapFaultDefinitionExceptionResolver;
+import rs.ac.uns.ftn.exception.ServiceFaultException;
+
+import java.util.Properties;
 
 /**
  * Created by Jasmina on 23/06/2017.
@@ -19,6 +25,23 @@ import org.springframework.xml.xsd.XsdSchema;
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
+    public SoapFaultMappingExceptionResolver exceptionResolver(){
+
+        final SoapFaultMappingExceptionResolver exceptionResolver = new DetailSoapFaultDefinitionExceptionResolver();
+
+        final SoapFaultDefinition faultDefinition = new SoapFaultDefinition();
+        faultDefinition.setFaultCode(SoapFaultDefinition.SERVER);
+        exceptionResolver.setDefaultFault(faultDefinition);
+
+        final Properties errorMappings = new Properties();
+        errorMappings.setProperty(Exception.class.getName(), SoapFaultDefinition.SERVER.toString());
+        errorMappings.setProperty(ServiceFaultException.class.getName(), SoapFaultDefinition.SERVER.toString());
+        exceptionResolver.setExceptionMappings(errorMappings);
+        exceptionResolver.setOrder(1);
+        return exceptionResolver;
+    }
+
+    @Bean
     public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
@@ -26,8 +49,25 @@ public class WebServiceConfig extends WsConfigurerAdapter {
         return new ServletRegistrationBean(servlet, "/ws/*");
     }
 
+
+    @Bean(name = "mt102")
+    public DefaultWsdl11Definition Mt102SchemaDefinition(XsdSchema mt102schema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("Mt102Port");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.ftn.uns.ac.rs/mt102");
+        wsdl11Definition.setSchema(mt102schema);
+        return wsdl11Definition;
+    }
+
+
+    @Bean
+    public XsdSchema mt102schema() {
+        return new SimpleXsdSchema(new ClassPathResource("mt102.xsd"));
+    }
+
     @Bean(name = "mt103")
-    public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema mt103schema) {
+    public DefaultWsdl11Definition Mt103SchemaDefinition(XsdSchema mt103schema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("Mt103Port");
         wsdl11Definition.setLocationUri("/ws");
@@ -37,7 +77,39 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     }
 
     @Bean
-    public XsdSchema countriesSchema() {
+    public XsdSchema mt103schema() {
         return new SimpleXsdSchema(new ClassPathResource("mt103.xsd"));
     }
+
+    @Bean(name = "mt900")
+    public DefaultWsdl11Definition Mt900SchemaDefinition(XsdSchema mt900schema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("Mt900Port");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.ftn.uns.ac.rs/mt900");
+        wsdl11Definition.setSchema(mt900schema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema mt900schema() {
+        return new SimpleXsdSchema(new ClassPathResource("mt900.xsd"));
+    }
+
+    @Bean(name = "mt910")
+    public DefaultWsdl11Definition Mt910SchemaDefinition(XsdSchema mt910schema) {
+        DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
+        wsdl11Definition.setPortTypeName("Mt910Port");
+        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setTargetNamespace("http://www.ftn.uns.ac.rs/mt910");
+        wsdl11Definition.setSchema(mt910schema);
+        return wsdl11Definition;
+    }
+
+    @Bean
+    public XsdSchema mt910schema() {
+        return new SimpleXsdSchema(new ClassPathResource("mt910.xsd"));
+    }
+
+
 }
